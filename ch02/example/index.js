2dd0f2bec2725e10e6c023e41caefe49f81b332c
Kassian Wren <the@nodebotani.st>
2023-01-07 13:07:51 -0600

    ch02

---
+++ ch02/example/index.js
@@ -0,0 +1,10 @@
+// bring in the compiled bytecode with a JS wrapper created for us by wasm-pack
+import init from './pkg/ch02.js'
+
+const runWasm = async () => {
+    // create a helloWorld object out of instantiated wasm code
+    const helloWorld = await init('./pkg/ch02_bg.wasm')
+    // call our hello_world function (console.log)
+    helloWorld.hello_world()
+}
+runWasm()
