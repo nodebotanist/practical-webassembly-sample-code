0bd78dd2d4137d9c4b4d4c41f599282f127c681e
Kassian Wren <the@nodebotani.st>
2023-02-02 13:33:31 -0600

    Got assemblyscript working

--- ch04/js/index.js
+++ ch04/js/index.js
@@ -1,16 +1,19 @@
+import {add} from '../build/release.js'
+
 (() => {
-const rollDiceButton = document.querySelector('#roll_dice')
-const rollDiceInput = document.querySelector('input[name=dice_text]')
-const errorDiv = document.querySelector('#error_message')
+    const rollDiceButton = document.querySelector('#roll_dice')
+    const rollDiceInput = document.querySelector('input[name=dice_text]')
+    const errorDiv = document.querySelector('#error_message')
 
-rollDiceButton.addEventListener('click', () => {
-    const verificationRegExp = /^[0-9]+[d][0-9]+(\+[0-9]+)?$/
-    if( rollDiceInput.value.match(verificationRegExp) == null ) {
-        errorDiv.innerHTML = `<p>Invalid input: must be in the format [number]d[number], with an optional +[number]</p>`
-    } else {
-        errorDiv.innerHTML = ``
-    }
-})
+    rollDiceButton.addEventListener('click', () => {
+        const verificationRegExp = /^[0-9]+[d][0-9]+(\+[0-9]+)?$/
+        if( rollDiceInput.value.match(verificationRegExp) == null ) {
+            errorDiv.innerHTML = `<p>Invalid input: must be in the format [number]d[number], with an optional +[number]</p>`
+        } else {
+            errorDiv.innerHTML = ``
+        }
+    })
 
-console.log(rollDiceButton, rollDiceInput)
+    const result = add(3,4)
+    console.log(result)
 })()
