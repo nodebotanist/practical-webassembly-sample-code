74554b76b5a15dcf553575ae0cf10e475448505c
Kassian Wren <the@nodebotani.st>
2023-02-03 09:22:30 -0600

    Example works

--- ch04/assembly/index.ts
+++ ch04/assembly/index.ts
@@ -4,11 +4,31 @@ export function add(a: i32, b: i32): i32 {
   return a + b;
 }
 
-export function readDiceInput(input: String): Array<i32> {
-  let splitForDie = input.split('d', 10)
-  let splitForExtra = splitForDie[1].split('+')
-  let numberOfDice = i32.parse(splitForDie[0], 10)
-  let numberOnDie = i32.parse(splitForExtra[0], 10)
-  let extraAdd = i32.parse(splitForExtra[1], 10)
+export function roll(input: String): i32 {
+  let numbersToRoll: Array<i32> = readDiceInput(input)
+  return rollDice(numbersToRoll)
+}
+
+function readDiceInput(input: String): Array<i32> {
+  let splitForDie: Array<String> = input.split('d', 10)
+  let splitForExtra: Array<String> = splitForDie[1].split('+')
+  let numberOfDice: i32 = i32.parse(splitForDie[0] as string, 10)
+  let numberOnDie: i32 = i32.parse(splitForExtra[0] as string, 10)
+  let extraAdd: i32= i32.parse(splitForExtra[1] as string, 10)
   return [numberOfDice, numberOnDie, extraAdd]
+}
+
+function rollDice(diceToRoll: Array<i32>): i32 {
+  let numberOfDice: i32 = diceToRoll[0]
+  let numberOnDie: i32 = diceToRoll[1]
+  let extraAdd: i32 = diceToRoll[2]
+  let total:i32 = 0
+  for(let i=0; i<numberOfDice; i++) {
+    total += rollDie(numberOnDie)
+  }
+  return total + extraAdd
+}
+
+function rollDie(dieMax: i32): i32 {
+  return Math.ceil((Math.random() * dieMax as f64)) as i32
 }
--- ch04/js/index.js
+++ ch04/js/index.js
@@ -1,9 +1,10 @@
-import {add, readDiceInput} from '../build/release.js'
+import {roll} from '../build/release.js'
 
 (() => {
     const rollDiceButton = document.querySelector('#roll_dice')
     const rollDiceInput = document.querySelector('input[name=dice_text]')
     const errorDiv = document.querySelector('#error_message')
+    const resultSpan = document.querySelector('#dice_result')
 
     rollDiceButton.addEventListener('click', () => {
         const verificationRegExp = /^[0-9]+[d][0-9]+(\+[0-9]+)?$/
@@ -11,11 +12,8 @@ import {add, readDiceInput} from '../build/release.js'
             errorDiv.innerHTML = `<p>Invalid input: must be in the format [number]d[number], with an optional +[number]</p>`
         } else {
             errorDiv.innerHTML = ``
-            let result = readDiceInput(rollDiceInput.value)
-            console.log(result)
+            let result = roll(rollDiceInput.value)
+            resultSpan.innerHTML = result
         }
     })
-
-    const result = add(3,4)
-    console.log(result)
 })()
