292ca9aae0ad52eeecd19fa91ba4134fa783643f
Kassian Wren <the@nodebotani.st>
2023-02-03 10:02:58 -0600

    removed unused add function

--- ch04/assembly/index.ts
+++ ch04/assembly/index.ts
@@ -1,9 +1,5 @@
 // The entry file of your WebAssembly module.
 
-export function add(a: i32, b: i32): i32 {
-  return a + b;
-}
-
 export function roll(input: String): i32 {
   let numbersToRoll: Array<i32> = readDiceInput(input)
   return rollDice(numbersToRoll)
