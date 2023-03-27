68e98941c41f6a609fd20aa95f7d2252705ec13d
Kassian Wren <the@nodebotani.st>
2023-03-27 12:20:30 -0500

    Cleanup

--- ch07/rust_example/target/debug/deps/rust_example-cc91e968537bcc51.d
+++
@@ -1,5 +0,0 @@
-/home/nodebotanist/book/practical-webassembly-sample-code/ch07/rust_example/target/debug/deps/rust_example-cc91e968537bcc51: src/main.rs
-
-/home/nodebotanist/book/practical-webassembly-sample-code/ch07/rust_example/target/debug/deps/rust_example-cc91e968537bcc51.d: src/main.rs
-
-src/main.rs:
--- ch07/rust_example/target/debug/rust_example.d
+++
@@ -1,1 +0,0 @@
-/home/nodebotanist/book/practical-webassembly-sample-code/ch07/rust_example/target/debug/rust_example: /home/nodebotanist/book/practical-webassembly-sample-code/ch07/rust_example/src/main.rs
