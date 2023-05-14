bf12df47b7df6344ed5d459b608762f4558002a9
Kassian Wren <the@nodebotani.st>
2023-05-14 09:51:18 -0500

    Working on regex support

---
+++ ch06/dice_roll/src/lib.rs
@@ -0,0 +1,20 @@
+use std::env;
+use regex::Regex;
+
+fn main(){
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        let roll = args[1].clone();
+
+        let rollRegex = Regex::new(r"^[0-9]+[d][0-9]+(\+[0-9]+)?$").unwrap();
+        if rollRegex.is_match(&roll) {
+            println!("Roll {} is valid", roll);
+        } else {
+            eprintln!("Roll {} is invalid, must be ##d##+##", roll);
+        }
+        
+    } else {
+        eprintln!("Error: need an argument dice roll ##d##+##");
+    }
+}
