4d8b42203fc3efd481834b62b7529806aa96b33a
Kassian Wren <the@nodebotani.st>
2024-05-07 13:58:32 -0500

    Finished ch12 code

--- ch12/wws-dice-roller/src/main.rs
+++ ch12/wws-dice-roller/src/main.rs
@@ -1,31 +1,33 @@
+use anyhow::Result;
+use wasm_workers_rs::{
+    worker,
+    http::{self, Request, Response},
+    Content,
+};
 extern crate regex;
 
-use spin_sdk::http::{IntoResponse, Request, Response};
-use spin_sdk::http_component;
 use rand::{thread_rng, Rng};
 use regex::Regex;
 
-#[http_component]
-fn handle_dice_roller(req: Request) -> anyhow::Result<impl IntoResponse> {
-    println!("Handling request to {:?}", req.header("spin-full-url"));
-    let roll_string = req.query();
+#[worker]
+fn reply(req: Request<String>) -> Result<Response<Content>> {
+    let roll_string = req.uri();
     let roll_regex = Regex::new(r"roll=([0-9]+)[d]([0-9]+)\+?([0-9]+)?").unwrap();
     // parse the roll out of the query string
-    let captures = roll_regex.captures(roll_string);
+    let captures = if roll_string.query() == None { None } else { roll_regex.captures(roll_string.query().unwrap()) };
     match captures {
         Some(ref roll_match) => {
             if roll_match.get(2) == None || roll_match.get(1) == None {
                 return Ok(Response::builder()
                 .status(400)
                 .header("content-type", "text/plain")
-                .body("Invalid roll")
-                .build())
+                .body(format!("Invalid roll").into())?);
             }
             // use the capture groups to pull the numbers we need out, then turn them into numbers
             let number_of_dice: i32 = roll_match.get(1).unwrap().as_str().parse().unwrap();
             let dice_max: i32 = roll_match.get(2).unwrap().as_str().parse().unwrap();
             let modifier: i32 = if roll_match.get(3) == None { 0 } else { roll_match.get(3).unwrap().as_str().parse().unwrap() };
-            println!("Dice roll numbers: {:?}, {:?}, {:?}", number_of_dice, dice_max, modifier);
+            // println!("Dice roll numbers: {:?}, {:?}, {:?}", number_of_dice, dice_max, modifier);
             // create dice roll using the numbers we parsed out
             let mut rng = thread_rng();
             // start the sum for the dice roll total with the modifier
@@ -40,21 +42,19 @@ fn handle_dice_roller(req: Request) -> anyhow::Result<impl IntoResponse> {
                 // add the die result to the total
                 dice_roll_total += this_dice_roll;
             }
-            println!("Dice roll total: {:?}, Rolls: {:?}", dice_roll_total, rolls);
+            // println!("Dice roll total: {:?}, Rolls: {:?}", dice_roll_total, rolls);
 
             // return an HTTP response with the total and the rolls
-            return Ok(Response::builder()
+            return Ok(http::Response::builder()
             .status(200)
             .header("content-type", "text/plain")
-            .body(format!("Dice roll total: {:?}, Rolls: {:?}", dice_roll_total, rolls))
-            .build());
+            .body(format!("Dice roll total: {:?}, Rolls: {:?}", dice_roll_total, rolls).into())?);
         }
         None => {
-            return Ok(Response::builder()
-            .status(400)
-            .header("content-type", "text/plain")
-            .body("Invalid roll")
-            .build());
+            return Ok(http::Response::builder()
+                .status(400)
+                .header("content-type", "text/plain")
+                .body(format!("Invalid roll").into())?);
         }
     }
 }
