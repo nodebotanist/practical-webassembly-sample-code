8e0d55e4abc66e200760dbb678d52a64aeb97fe5
Kassian Wren <the@nodebotani.st>
2024-05-17 09:17:44 -0500

    ch11: form button pulls up values

--- ch11/yew-app/src/main.rs
+++ ch11/yew-app/src/main.rs
@@ -1,5 +1,5 @@
 extern crate web_sys;
-use web_sys::{console, window, HtmlInputElement};
+use web_sys::{console, window, HtmlInputElement, MouseEvent};
 use yew::{
     Html,
     html,
@@ -49,6 +49,32 @@ fn App() -> Html {
             .value();
         console::log_1(&format!("Changed dice modifier: {:?}", input_value).into());
     };
+
+    let on_dice_roll = move |_:MouseEvent| {
+        let window = web_sys::window().expect("no global `window` exists");
+        let document = window.document().expect("should have a document on window");
+        let body = document.body().expect("document should have a body");
+        let number_of_dice = document
+            .get_element_by_id("num_of_dice")
+            .unwrap()
+            .dyn_into::<HtmlInputElement>()
+            .unwrap()
+            .value();
+        let dice_max_value = document
+            .get_element_by_id("dice_max_value")
+            .unwrap()
+            .dyn_into::<HtmlInputElement>()
+            .unwrap()
+            .value();
+        let dice_modifier = document
+            .get_element_by_id("dice_modifier")
+            .unwrap()
+            .dyn_into::<HtmlInputElement>()
+            .unwrap()
+            .value();
+        console::log_1(&format!("Roll {:?}d{:?}+{:?}", number_of_dice, dice_max_value, dice_modifier).into());
+    };
+
     html! {
         <div>
             <label for="number_of_dice">{"Number of Dice:"}</label>
@@ -56,7 +82,9 @@ fn App() -> Html {
             <label for="number_of_dice">{"Max value of Dice:"}</label>
             <input type="number" id="dice_max_value" name="dice_max_value" placeholder=1 min="1" max="100" onchange={onchange_dice_max_value} /><br />
             <label for="number_of_dice">{"Dice Roll Modifier:"}</label>
-            <input type="number" id="dice_modifier" name="dice_modifier" placeholder=0 min="-100" max="100" onchange={onchange_modifier} />
+            <input type="number" id="dice_modifier" name="dice_modifier" placeholder=0 min="-100" max="100" onchange={onchange_modifier} /><br />
+            <button id="roll_dice_betton" onclick={on_dice_roll} type="submit" >{"Roll Dice"}</button>
+            <div id ="dice_roll_results"></div>
         </div>
     }
 }
