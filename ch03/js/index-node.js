7c5356b8b0b5852ba437761ef051de51d8c00cf7
Kassian Wren <the@nodebotani.st>
2024-08-14 07:08:26 -0500

    Ch03 node code

---
+++ ch03/js/index-node.js
@@ -0,0 +1,23 @@
+const fs = require('node:fs')
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+})
+
+const wasmBuffer = fs.readFileSync(`../build/stat_bonus_calculator.wasm`)
+
+WebAssembly.instantiate(wasmBuffer).then(
+    (wasm) => {
+        console.log("Enter a stat amount")
+        rl.on(`line`, (line) => {
+            if(isNaN(line)){
+                console.log("Please enter a number")
+            } else {
+                console.log(`Result: ${wasm.instance.exports.calculate_stat_bonus(parseInt(line))}`)
+            }
+        })
+    }
+)
