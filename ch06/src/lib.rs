2706f93c2c3f48d7f9607c93840461a2a0dd0157
Kassian Wren <the@nodebotani.st>
2024-09-13 20:50:31 -0500

    Rust updates work

--- ch06/src/lib.rs
+++ ch06/src/lib.rs
@@ -1,20 +1,39 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use getrandom::getrandom;
+use regex::Regex;
 
 #[wasm_bindgen(module = "/lib/ch04/release.js")]
 extern "C" {
-    #[wasm_bindgen(js_name = "roll_die")]
-    fn roll_die(dieMax: i32) -> i32;
-
     #[wasm_bindgen(js_name = "parse_roll_string")]
     fn parse_roll_string(roll_string: &str) -> js_sys::Array;
 }
 
 #[wasm_bindgen]
 pub fn validate_roll_string(roll_string: &str) -> bool {
-    let reg_exp = js_sys::RegExp::new(r"^[0-9]+[d][0-9]+((\+|-)[0-9]+)?$", "");
-    reg_exp.test(roll_string)
+    // upgrade this to use a rust regex
+    let reg_exp = Regex::new(r"^[0-9]+[d][0-9]+((\+|-)[0-9]+)?$").unwrap();
+    match reg_exp.captures(roll_string) {
+        None => false,
+        _ => true
+    }
+}
+
+fn roll_die(die_max: i32) -> i32 {
+    // generate a random number between 1 and die_max inclusively
+    let mut rand:[u8;1] = [0];
+    let random_result = getrandom(&mut rand);
+    match random_result {
+        Ok(()) => {
+            // rand[0] is now a random u8. To get the range, we divide by the max value and multiply by die_max
+            let ratio = rand[0] as f32 / u8::MAX as f32;
+            let result = (ratio * die_max as f32).ceil() as i32;
+            result
+        }
+        Err(_) => panic!("Error getting random bytes")
+    }
+
 }
 
 #[wasm_bindgen]
@@ -45,4 +64,17 @@ pub fn roll_dice(roll_string: &str) -> Result<i32, JsError> {
 
     // return the total
     Ok(total)
+}
+
+#[wasm_bindgen]
+pub fn roll_dice_log(roll_string: &str) {
+    let result = roll_dice(roll_string);
+
+    match result {
+        Ok(roll_result) => console::log_1(&format!("Roll: {:?}, Result: {:?}", roll_string, roll_result).into()),
+        Err(_) => panic!("Error getting the roll result")
+    }
+    // console log result
+    use web_sys::console;
+    
 }
