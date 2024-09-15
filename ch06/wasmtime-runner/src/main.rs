66094fa1db56a80d32c23f7a2701e63f5404519a
Kassian Wren <the@nodebotani.st>
2024-09-14 20:33:19 -0500

    Ch 06 code

--- ch06/wasmtime-runner/src/main.rs
+++ ch06/wasmtime-runner/src/main.rs
@@ -1,5 +1,7 @@
 extern crate roll_checker;
 use std::env;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
 
 #[no_mangle]
 pub fn main() {
@@ -9,10 +11,25 @@ pub fn main() {
 #[no_mangle]
 pub extern "C" fn roll_dice_log() {
     let args: Vec<String> = env::args().collect();
-    let number_of_dice = args[1].clone().parse().unwrap();
-    let die_max = args[2].clone().parse().unwrap();
-    let modifier = args[3].clone().parse().unwrap();
 
-    let result = roll_checker::roll_dice_from_numbers(number_of_dice, die_max, modifier);
-    println!("Result of {:?}d{:?}+{:?}: {:?}", number_of_dice, die_max, modifier, result)
+    if args.len() >= 4 {
+        let number_of_dice = args[1].clone().parse().unwrap();
+        let die_max = args[2].clone().parse().unwrap();
+        let modifier = args[3].clone().parse().unwrap();
+
+        let result = roll_checker::roll_dice_from_numbers(number_of_dice, die_max, modifier);
+        println!("Result of {:?}d{:?}+{:?}: {:?}", number_of_dice, die_max, modifier, result);
+
+        let mut roll_log_file = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open("./roll-log.txt")
+            .unwrap();
+
+        if let Err(e) = writeln!(roll_log_file, "Result of {:?}d{:?}+{:?}: {:?}", number_of_dice, die_max, modifier, result) {
+            panic!("Error writing to file: {:?}", e);
+        }
+    } else {
+        panic!("Need 3 integer arguments ## ## ##");
+    }
 }
